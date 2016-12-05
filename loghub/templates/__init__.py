1d884e267682ffed36655147cf2a6a8ccb3fe335
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2016-12-04 22:45:58 -0500

    Add default templates

---
+++ loghub/templates/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright (c) The Spyder Development Team
+#
+# Licensed under the terms of the MIT License
+# (See LICENSE.txt for details)
+# -----------------------------------------------------------------------------
+"""Changelog generator based on github milestones or tags."""
+
+# Standar library imports
+import os
+
+HERE = os.path.dirname(os.path.realpath(__file__))
+CHANGELOG_TEMPLATE_PATH = os.path.join(HERE, 'changelog.txt')
+RELEASE_TEMPLATE_PATH = os.path.join(HERE, 'release.txt')
