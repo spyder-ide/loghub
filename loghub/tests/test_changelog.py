06a3be438f01a13d02d7d0e8be1a5af40f748395
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2016-12-05 23:34:15 -0500

    Add branch test

--- loghub/tests/test_changelog.py
+++ loghub/tests/test_changelog.py
@@ -81,3 +81,31 @@ In this release 1 pull request was closed.
     print([log])
     print([expected])
     assert log == expected
+
+
+@pytest.mark.skipif(NOT_ON_CI, reason='test on ci server only')
+def test_changelog_release_branch():
+    log = create_changelog(
+        repo=REPO,
+        token=TEST_TOKEN,
+        milestone=TEST_MILESTONE,
+        branch='test-branch',
+        output_format='release')
+    expected = '''## Version <RELEASE_VERSION> (2016-12-05)
+
+### Issues Closed
+
+* Issue #26 - Test number 2
+* Issue #24 - Issue test
+
+In this release 2 issues were closed.
+
+### Pull Requests Merged
+
+* PR #33 - PR: Test change
+
+In this release 1 pull request was closed.
+'''
+    print([log])
+    print([expected])
+    assert log == expected
