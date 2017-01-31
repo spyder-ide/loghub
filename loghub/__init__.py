df9587e98510f9f088dea9bacaa9e424352bd144
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2017-01-31 14:48:30 -0500

    Restore dev version

--- loghub/__init__.py
+++ loghub/__init__.py
@@ -7,5 +7,5 @@
 # -----------------------------------------------------------------------------
 """Changelog generator based on github milestones or tags."""
 
-VERSION_INFO = (0, 2)
+VERSION_INFO = (0, 2, 1, 'dev0')
 __version__ = '.'.join(map(str, VERSION_INFO))
