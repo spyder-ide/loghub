3acb1b4e7d9f313ed40e519b50499455577bab13
Carlos Cordoba <ccordoba12@gmail.com>
2018-02-18 13:57:51 -0500

    Set release version

--- loghub/__init__.py
+++ loghub/__init__.py
@@ -7,5 +7,5 @@
 # -----------------------------------------------------------------------------
 """Changelog generator based on github milestones or tags."""
 
-VERSION_INFO = (0, 4, 0, 'dev0')
+VERSION_INFO = (0, 3, 2)
 __version__ = '.'.join(map(str, VERSION_INFO))
