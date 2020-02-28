cc1dad75edd0d1c9eb4c1b6aef7bbd8777b36e9b
goanpeca <goanpeca@gmail.com>
2020-02-27 22:26:24 -0500

    Complete docstrings

--- loghub/tests/utils.py
+++ loghub/tests/utils.py
@@ -8,9 +8,8 @@
 """Tests utils."""
 
 
-# --- Mocks
-# -----------------------------------------------------------------------------
 class Issue:
+    """Issue github mock."""
 
     def __init__(self, number, full_repo='foo/bar', body=None, is_pr=False):
         self.pull_request = is_pr
@@ -28,14 +27,14 @@ class Issue:
     def __str__(self):
         return self.__repr__()
 
-    def get(self, name, default=None):
-        if default is None:
-            return self.__dict__.get(name)
-        else:
-            return self.__dict__.get(name, default)
-
     def __getitem__(self, key):
         return self.__dict__[key]
 
     def __setitem__(self, key, value):
         self.__dict__[key] = value
+
+    def get(self, name, default=None):
+        if default is None:
+            return self.__dict__.get(name)
+        else:
+            return self.__dict__.get(name, default)
